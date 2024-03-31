a04ddb5c9d726372aa045cc514e57de5ada5b1be
YuriODantas <iruy3343@gmail.com>
2024-03-31 17:45:32 -0300

    feat: create functions to add or update itens in cartContext of the home page

--- src/components/CardFilm/index.tsx
+++ src/components/CardFilm/index.tsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react'
+
 import { ResponseGetFilms } from '../../api/films'
+import { CartContext } from '../../context/CartContext'
+import { priceFormatter } from '../../utils/formatter'
 import {
   CardFilmButton,
   CardFilmContainer,
@@ -11,19 +15,25 @@ interface IProps {
 }
 
 export function CardFilm({ card }: IProps) {
-  const quantity = 0
+  const { cartItems, addNewItemInCart, updateQuantityInItemCart } =
+    useContext(CartContext)
+  const quantity = cartItems.find((item) => item.id === card.id)?.quantity || 0
   const activeButton: boolean = quantity > 0
+
+  const handleClick = () => {
+    if (cartItems.some((item) => item.id === card.id)) {
+      updateQuantityInItemCart({ id: card.id, quantity: quantity + 1 })
+      return
+    }
+    addNewItemInCart({ ...card, quantity: 1 })
+  }
+
   return (
     <CardFilmContainer>
       <img src={card.image} alt="" />
       <CardFilmTitle>{card.title}</CardFilmTitle>
-      <CardFilmPrice>
-        {card.price.toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-        })}
-      </CardFilmPrice>
-      <CardFilmButton isActive={activeButton}>
+      <CardFilmPrice>{priceFormatter.format(card.price)}</CardFilmPrice>
+      <CardFilmButton isActive={activeButton} onClick={() => handleClick()}>
         <img src="/cart-button-icon.svg" alt="card button icon" />
         <span>{quantity}</span>
         <p>Adicionar ao carrinho</p>
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,11 +1,13 @@
+import { useTotalQuantityItems } from '../../hook/useTotalQuantityItems'
 import { CartContainerStyle, CartTextStyle } from './styles'
 
 export function Cart() {
+  const total = useTotalQuantityItems()
   return (
     <CartContainerStyle to="/cart">
       <CartTextStyle>
         <p>Meu Carrinho</p>
-        <span>0 itens</span>
+        <span>{total} itens</span>
       </CartTextStyle>
       <img src="/cart-icon.svg" alt="cart icon" />
     </CartContainerStyle>
