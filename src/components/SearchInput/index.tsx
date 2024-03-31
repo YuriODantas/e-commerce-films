25771790bea11d2d8e1e58d4b8cf1dee9db786b2
YuriODantas <iruy3343@gmail.com>
2024-03-31 15:08:04 -0300

    feat: create function to search film

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
+
 import { SearchInputContainer } from './styles'
 
-export function SearchInput() {
+interface IProps {
+  handleSearch: (query: string) => void
+  valueSearch: string
+}
+
+export function SearchInput({ handleSearch, valueSearch }: IProps) {
+  const [inputValue, setInputValue] = useState<string>(valueSearch)
+
   return (
     <SearchInputContainer>
-      <input type="text" placeholder="Buscar filme pelo nome" />
-      <button>
+      <input
+        type="text"
+        placeholder="Buscar filme pelo nome"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onBlur={() => handleSearch(inputValue)}
+      />
+      <button onClick={() => handleSearch(inputValue)}>
         <img src="/search-icon.svg" alt="search icon" />
       </button>
     </SearchInputContainer>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,51 @@
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
+import { useSearchParams } from 'react-router-dom'
 
+import { getFilms } from '../api/films'
+import { CardFilm } from '../components/CardFilm'
+import { Loader } from '../components/Loader'
 import { SearchInput } from '../components/SearchInput'
+import { WrapperCards } from '../components/WrapperCards'
 
 export function Home() {
+  const queryName = 'search-query'
+  const [searchParams, setSearchParams] = useSearchParams()
+  const findFilm = searchParams.get(queryName)
+
+  const { data: listFilms, isFetching } = useQuery({
+    queryKey: ['list', findFilm],
+    queryFn: () => getFilms(findFilm),
+  })
+
+  function handleSearch(query: string) {
+    setSearchParams((state) => {
+      if (query.length === 0) {
+        state.delete(queryName)
+      } else {
+        state.set(queryName, query)
+      }
+      return state
+    })
+  }
+
   return (
     <>
       <Helmet title="Products" />
-      <SearchInput />
+      {isFetching && <Loader />}
+      {!isFetching && (
+        <>
+          <SearchInput
+            handleSearch={handleSearch}
+            valueSearch={findFilm === null ? '' : findFilm}
+          />
+          <WrapperCards>
+            {listFilms?.data.map((film) => {
+              return <CardFilm key={film.id} card={film} />
+            })}
+          </WrapperCards>
+        </>
+      )}
     </>
   )
 }
